1e4d8dad09eb96be39e10a62f83dede6a97c9cf6
Matt Copperwaite <matt@matt-u36sd.local>
2012-01-04 19:31:02 +0000

    Created script to download latest jQuery

---
+++ download-frameworks.sh
@@ -0,0 +1,4 @@
+wget --directory-prefix=static/js/ http://code.jquery.com/jquery.min.js
+#wget -O static/css/blueprint.tar.gz http://github.com/joshuaclayton/blueprint-css/tarball/master 
+#echo "You will need to extract blueprint files yourself."
+echo "Done."
